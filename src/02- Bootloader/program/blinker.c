30ccd0b44c3509bed04027a62472f774f337c200
Ahmed Amrani Akdi <ahmed-trabajos@hotmail.com>
2020-10-18 02:26:50 +0100

    finished bootloader

--- src/02- Bootloader/program/blinker.c
+++ src/02- Bootloader/program/blinker.c
@@ -5,18 +5,18 @@
 
 
 int notmain(){
-    //gpio_set_output(GPIO_PIN16);
-    unsigned int ra = GET32(GPFSEL2);
+    gpio_set_output(GPIO_PIN16);
+    /*unsigned int ra = GET32(GPFSEL1);
     ra&=~(7<<18);
     ra|=1<<18;
-    PUT32(GPFSEL2,ra);
+    PUT32(GPFSEL1,ra);*/
     //int i = 0;
     while(1){
-        //gpio_write(GPIO_PIN16, 1);
-        PUT32(GPSET0,1<<16);
+        gpio_write(GPIO_PIN16, 1);
+        //PUT32(GPSET0,1<<16);
         delay_ms(500);
-        //gpio_write(GPIO_PIN16, 0);
-        PUT32(GPCLEAR0,1<<16);
+        gpio_write(GPIO_PIN16, 0);
+        //PUT32(GPCLEAR0,1<<16);
         delay_ms(500);
         //i++;
     }
